45844bcab09e22e6bbf1e6ba0ed05bf6d4be747c
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-03-30 17:03:09 +0100

    Stage 10 completed

---
+++ spec/bike_spec.rb
@@ -0,0 +1,5 @@
+# require 'docking_station'
+
+# RSpec.describe Bike do
+#   it { is_expected.to respond_to :working? }
+# end
--- spec/boris_bike_spec.rb
+++ spec/boris_bike_spec.rb
@@ -1,7 +1,18 @@
 require 'docking_station'
 
-describe DockingStation do 
-  it 'should initialize a dockingstation' do 
-    
+RSpec.describe DockingStation do 
+  before do
+    @docking_station = DockingStation.new
+
   end
+  it { is_expected.to respond_to :release_bike }
+  # it { expect(@docking_station.release_bike).to eq(Bike.new) }
+  it 'release working bike' do
+    bike = @docking_station.release_bike
+    expect(bike.working?).to be_truthy
+  end
+end
+
+RSpec.describe Bike do
+  it { is_expected.to respond_to :working? }
 end
