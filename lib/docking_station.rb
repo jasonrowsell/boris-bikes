c70cce4620ba6379f0bf2bb57afa07201ea427d4
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-03-30 18:02:27 +0100

    Stages 1-10 complete

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,19 @@
 class DockingStation
-  def release_bike
-     Bike.new
+  attr_reader :bike
+
+  def initialize
+    @bike = []
   end
+
+  def release_bike
+    Bike.new
+  end
+
+  #dock needs to store bike-instance's ID and working
+  def dock(bike)
+    @bike << bike
+  end
+
 end
 
 class Bike
