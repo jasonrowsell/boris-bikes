2514789ee51b5a38e4df3739260dcdd2ca520931
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-03-31 15:44:10 +0100

    Refactored RSpec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,23 +5,25 @@ RSpec.describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases a working bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    expect(subject.release_bike).to be_working
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it 'docks a bike' do 
+  it { is_expected.to respond_to(:bike) }
+
+  it 'returns a docked bike' do 
     bike = Bike.new
-    expect(subject.dock(bike)).to eq(bike)
+    subject.dock(bike)
+    expect(subject.bike).to eq(bike)
   end
 
-  describe '#release_bike' do
-    it 'releases a docked bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq(bike)
-    end     
-  end
+  # describe '#release_bike' do
+  #   it 'releases a docked bike' do
+  #     bike = Bike.new
+  #     subject.dock(bike)
+  #     expect(subject.release_bike).to eq(bike)
+  #   end     
+  # end
 
 end
